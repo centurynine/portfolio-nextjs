3a11605d478fdd081edb928f9e8495b8c7d9c314
CenturyNine <lowlifeix@gmail.com>
2023-09-06 21:58:47 +0700

    up

--- pages/data/constants.js
+++ pages/data/constants.js
@@ -16,7 +16,7 @@ export const MyBio = {
       "Flutter Developer.",
     ],
     description:
-      "I'm interested in Node.js for creating diverse and user-friendly web applications, and I aim to excel in both frontend and backend development. This journey will shape me into a proficient FullStack Developer, poised to excel in all aspects of app development.",
+      "I am interested in creating diverse and user-friendly web applications. I aim to excel in both frontend and backend development. This journey will help me become a proficient full-stack developer, poised to excel in all aspects of app development.",
     github: "https://github.com/centurynine",
     resume:
       "https://drive.google.com/file/d/119bFl8jNC__BTKHBqY5OnyYl9pfsIRY2/view?usp=drive_link",
